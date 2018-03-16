944b208685475dd7203a932f3f762b6e405cca34
Jeffrey Huy Trung Pham <jeffrhp1@odin.ics.uci.edu>
2018-03-15 21:25:14 -0700

    initial

---
+++ Timer.h
@@ -0,0 +1,8 @@
+// Timer.h
+
+int Timer_start();
+int Timer_elapsedWallclockTime(double *wc);
+int Timer_elapsedUserTime(double *ut);
+int Timer_elapsedSystemTime(double *sys);
+int Timer_elapsedTime(double *wc, double *us, double *sys);
+
